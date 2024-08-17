20d80b22d0feb56426937fa7f122a86b85ab30fa
Wilson Ramos <hupcontato@gmail.com>
2024-08-17 18:47:34 -0300

    Ultimos Ajustes

--- src/services/api.js
+++ src/services/api.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 const api = axios.create({
 
-    baseURL:"http://192.168.0.104:3333"
-    // baseURL:"https://cadesapi.onrender.com"
+    // baseURL:"http://192.168.0.104:3333"
+    baseURL:"https://cadesapi.onrender.com"
 
     // npx react-native build-android --mode=release
 })
