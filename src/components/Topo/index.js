639d68cb523b7f7fd913fe50d087ba97d281dd5a
Wilson Ramos <hupcontato@gmail.com>
2024-08-26 17:49:39 -0300

    Ultimos Ajustes

--- src/components/Topo/index.js
+++ src/components/Topo/index.js
@@ -1,10 +1,11 @@
-import { View } from 'react-native';
+import { View, Dimensions } from 'react-native';
 import Icone from '../Icone';
 import Texto from '../Texto';
 import { useTheme } from '@react-navigation/native';
 
 export default function Topo({ children, iconeLeft, iconeRight, right, titulo, posicao }) {
 
+    const { width } = Dimensions.get("window")
     const { colors } = useTheme()
 
     return (
@@ -13,9 +14,9 @@ export default function Topo({ children, iconeLeft, iconeRight, right, titulo, p
             <View style={{ backgroundColor: colors.theme, height: 60, alignItems: 'center', flexDirection: 'row', justifyContent: 'space-between' }}>
                 <View style={{ flexDirection: "row", alignItems: "center", gap: 16 }}>
                     <Icone onpress={iconeLeft?.acao} nomeDoIcone={iconeLeft?.nome} />
-                    {posicao === 'left' ? <Texto cor='#fff' texto={titulo} tamanho={18} tipo='Medium' /> : null}
+                    {posicao === 'left' ? <Texto cor='#fff' texto={titulo.substring(0, 26)} tamanho={18} tipo='Medium' /> : null}
                 </View>
-                {posicao === 'center' ? <Texto cor='#fff' texto={titulo} tamanho={18} tipo='Medium' /> : null}
+                {posicao === 'center' ? <Texto cor='#fff' texto={titulo.substring(0, 26)} tamanho={18} tipo='Medium' /> : null}
 
                 <Icone onpress={iconeRight?.acao} nomeDoIcone={iconeRight?.nome} />
 
